ecbbe20514d35a8c314bd2c8813ee9d698d0f709
MaestroWell <budanova.k12@gmail.com>
2024-02-25 12:34:51 +0300

    first commit

---
+++ main.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+export (PackedScene) var Drop
+
+func _ready():
+	randomize()
+	
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_coin_timer_timeout():
+	pass # Replace with function body.
